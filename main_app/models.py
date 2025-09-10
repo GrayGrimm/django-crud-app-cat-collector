c79272a16418bb90cee3d3a4899d2ce49b9cedae
guyera <thegraygrimm@graymansurvivaltech.com>
2025-09-10 13:18:04 -0400

    added a second model for feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.urls import reverse
+
 # Create your models here.
 from django.db import models
+
+MEALS = (("B", "Breakfast"), ("L", "Lunch"), ("D", "Dinner"))
+
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -9,9 +13,21 @@ class Cat(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
-     # new code below
+    # new code below
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('cat-detail', kwargs={'cat_id': self.id})
+        return reverse("cat-detail", kwargs={"cat_id": self.id})
+
+
+class Feeding(models.Model):
+    date = models.DateField("Feeding Date")
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
