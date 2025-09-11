28d3ed2c81b623678636526d95a78b9372a38b6b
guyera <thegraygrimm@graymansurvivaltech.com>
2025-09-11 12:38:18 -0400

    added toy model and a many to many association for toys.

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView # add these 
+from django.views.generic import ListView, DetailView  # add these
 from .models import Cat, Toy
 from .forms import FeedingForm
 
@@ -24,6 +24,7 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=cat.toys.all().values_list("id"))
     feeding_form = FeedingForm()
     return render(
         request,
@@ -31,13 +32,14 @@ def cat_detail(request, cat_id):
         {
             "cat": cat,
             "feeding_form": feeding_form,
+            "toys": toys_cat_doesnt_have,
         },
     )
 
 
 class CatCreate(CreateView):
     model = Cat
-    fields = "__all__"
+    fields = ["name", "breed", "description", "age"]
 
 
 class CatUpdate(UpdateView):
@@ -64,16 +66,32 @@ class ToyCreate(CreateView):
     model = Toy
     fields = "__all__"
 
+
 class ToyList(ListView):
     model = Toy
 
+
 class ToyDetail(DetailView):
     model = Toy
-    
+
+
 class ToyUpdate(UpdateView):
     model = Toy
-    fields = ['name', 'color']
+    fields = ["name", "color"]
+
 
 class ToyDelete(DeleteView):
     model = Toy
-    success_url = '/toys/'
+    success_url = "/toys/"
+
+
+def associate_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect("cat-detail", cat_id=cat_id)
+
+
+def remove_toy(request, cat_id, toy_id):
+    cat = Cat.objects.get(id=cat_id)
+    toy = Toy.objects.get(id=toy_id)
+    cat.toys.remove(toy)
+    return redirect("cat-detail", cat_id=cat.id)
